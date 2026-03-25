a2863b6b078db47ffae479d0837bac0e40daea23
wjdqudwls <wjdqudwls100@gmail.com>
2026-03-25 17:24:12 +0900

    chore: add CQRS skeleton packages

---
+++ src/main/java/com/conk/notification/Team1BackendNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.conk.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1BackendNotificationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Team1BackendNotificationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/conk/notification/Team1BackendNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conk.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1BackendNotificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
